19a95dc2956f9af9a3fe44d7212ddb590332f661
PAMLBL <pauline.loyer@gmail.com>
2024-04-30 14:59:53 +0200

    Update model.ts

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,3 +1,5 @@
+import { Time } from "@angular/common";
+
 export class Produit {
   public id?: number;
   public libelle?: string;
@@ -74,5 +76,71 @@ export class Formule {
     this.libelle = libelle;
     this.prix = prix;
     this.typeProduits = typeProduits;
+  }
+}
+
+export class DetailCommande {
+  public id?:number;
+  public prix?:number;
+  public qte?:number;
+  public commande?:Commande;
+  public produit?: Produit;
+  public menu?: Menu;
+
+  constructor(id? : number, prix?: number, qte?:number, commande?:Commande, produit?:Produit, menu?:Menu){
+    this.id = id;
+    this.prix = prix;
+    this.qte = qte;
+    this.commande = commande;
+    this.produit = produit;
+    this.menu = menu;
+  }
+}
+
+export class Commande {
+  public id? : number;
+  public client? : Utilisateur;
+  public avis? : Avis;
+  public detailCommandes? : Array<DetailCommande>;
+  public commentaire? : string;
+  public dateCommande? : Date;
+  public heureCommande? : Time;
+
+  constructor(id? : number, client?: Utilisateur, avis? : Avis, detailCommandes? : Array<DetailCommande>, commentaire?:string,dateCommande? : Date, heureCommande? : Time){
+    this.id = id;
+    this.client = client;
+    this.avis = avis;
+    this.detailCommandes = detailCommandes;
+    this.commentaire = commentaire;
+    this.dateCommande = dateCommande;
+    this.heureCommande = heureCommande;
+  }
+}
+
+export class Menu{
+  public id? : number;
+  public produits? : Array<Produit>;
+  public formule? : Formule;
+
+  constructor(id?:number,produits?:Array<Produit>,formule?:Formule){
+    this.id = id;
+    this.produits = produits;
+    this.formule = formule;
+  }
+}
+
+export class Avis{
+  public id?:number;
+  public avis? : string;
+  public commande? : Commande;
+  public heure? : Time;
+  public date? : Date;
+
+  constructor(id?:number,avis?:string,commande?:Commande,heure?:Time,date?:Date){
+    this.id = id;
+    this.avis = avis;
+    this.commande = commande;
+    this.heure = heure;
+    this.date = date;
   }
 }
