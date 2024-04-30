52272bbbecc0a7a4f8541927ca00aac8fadb9d1d
PAMLBL <pauline.loyer@gmail.com>
2024-04-30 09:38:33 +0200

    rajout model.ts appcomponent.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AjouterProduitComponent } from './ajouter-produit/ajouter-produit.component';
 import { ProduitComponent } from './produit/produit.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent, AjouterProduitComponent, ProduitComponent],
+  declarations: [AppComponent, AjouterProduitComponent, ProduitComponent, LoginComponent, SignUpComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/model.ts
+++ src/app/model.ts
@@ -47,3 +47,22 @@ enum typeProduit {
   dessert,
   boisson,
 }
+
+export class Utilisateur {
+  public id?: number;
+  public username?: string;
+  public password?: string;
+  public gestionnaire?: boolean;
+
+  constructor(
+    id?: number,
+    username?: string,
+    password?: string,
+    gestionnaire?: boolean
+  ) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.gestionnaire = gestionnaire;
+  }
+}
