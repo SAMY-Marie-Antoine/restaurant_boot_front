c9eb45f8a7f700d800b21975aaa15b00a84882c7
PAMLBL <pauline.loyer@gmail.com>
2024-04-30 10:12:23 +0200

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AjouterProduitComponent } from './ajouter-produit/ajouter-produit.component';
 import { ProduitComponent } from './produit/produit.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'ajouterProduit', component: AjouterProduitComponent },
   { path: 'produit', component: ProduitComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
 ];
 
 @NgModule({
